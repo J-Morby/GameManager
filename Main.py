f8ae7c9f47b0d83a8043ed2135fcd53054671971
Jack <jmm23@abbr.ac.uk>
2025-06-01 21:23:06 +0100

    ,

--- Main.py
+++ Main.py
@@ -1,9 +1,20 @@
 from tkinter import *
 
+class GameObject:
+
+    def __init__(self):
+        self.icon = ""
+        self.location = ""
+        self.x = 0
+        self.y = 0
+
+
+
 WindowName = "GameManager"
 
 window = Tk()
 
 window.title(f"{WindowName}")
+window.geometry('500x500')
 
 window.mainloop()
